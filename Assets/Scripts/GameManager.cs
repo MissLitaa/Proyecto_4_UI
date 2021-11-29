a6e951ee7750fb5619d3bdec401e3d2797820cfc
MissLitaa <anahug49@gmail.com>
2021-11-29 12:53:25 +0100

    ui
    ui

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,19 +12,30 @@ public class GameManager : MonoBehaviour
     private float minYPosition = -3.75f;
     private float spaceBetweenSquares = 2.5f;
     private int amountRows = 4;
-    private float spawnRate = 2f;
+    private float spawnRate = 0.5f;
+
+    public List<Vector3> targetPositions;
 
     private void Start()
     {
         StartCoroutine("SpawnRandomTarget");
     }
 
+    private void Update()
+    {
+        if (isGameOver==true)
+        {
+            Time.timeScale = 0;
+        }
+    }
+
     public Vector3 RandomSpawnPoint()
     {
         //Generamos las posiciones aleatorias en los 16 cuadrados (centros).
-        int randomInt = Random.Range(0, amountRows);
-        float randomPosX = minXPosition + randomInt * spaceBetweenSquares;
-        float randomPosY = minYPosition + randomInt * spaceBetweenSquares;
+        int randomIntX = Random.Range(0, amountRows);
+        int randomIntY = Random.Range(0, amountRows);
+        float randomPosX = minXPosition + randomIntX * spaceBetweenSquares;
+        float randomPosY = minYPosition + randomIntY * spaceBetweenSquares;
 
         //Guardamos los resultados de arriba en el RandomSpawnPoint gracias al return y al pasar esta función, nos irá haciendo new Vectors.
         return new Vector3(randomPosX, randomPosY, 0);
@@ -32,10 +43,21 @@ public class GameManager : MonoBehaviour
     
     private IEnumerator SpawnRandomTarget()
     {
-        yield return new WaitForSeconds(spawnRate);
-        int randomIndex = Random.Range(0, targetPrefabs.Length);
-        spawnPoint = RandomSpawnPoint();
-        Instantiate(targetPrefabs[randomIndex], spawnPoint, targetPrefabs[randomIndex].transform.rotation);
+        while (!isGameOver)
+        {
+            yield return new WaitForSeconds(spawnRate);
+
+            int randomIndex = Random.Range(0, targetPrefabs.Length);
+            spawnPoint = RandomSpawnPoint();
+
+            while (targetPositions.Contains(spawnPoint))
+            {
+                spawnPoint = RandomSpawnPoint();
+            }
+
+            Instantiate(targetPrefabs[randomIndex], spawnPoint, targetPrefabs[randomIndex].transform.rotation);
+            targetPositions.Add(spawnPoint);
+        }
 
     }
 
--- Assets/Scripts/target.cs
+++ Assets/Scripts/target.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class target : MonoBehaviour
 {
     private float destroyTimer = 2f;
+    private GameManager gameManagerScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, destroyTimer);
+        gameManagerScript = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Destroy(gameObject, destroyTimer);
     }
 
     private void OnMouseDown()
@@ -23,13 +25,21 @@ public class target : MonoBehaviour
         {
             Destroy(gameObject); 
             //Dar puntos, SP, AS.
+            
         }
 
         else if (gameObject.CompareTag("Bad"))
         {
             //Parar juego o restar puntos, o no contar puntuación, quitar vida, musiquita de game over o mal hecho, SP.
             Destroy(gameObject);
+            gameManagerScript.isGameOver = true;
+            
         }
         
+    }
+
+    private void OnDestroy()
+    {
+        gameManagerScript.targetPositions.Remove(transform.position);
     }
 }
